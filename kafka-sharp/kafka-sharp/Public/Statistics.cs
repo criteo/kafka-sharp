9b4f5ce314606c4ecc8e7208d7e9609ea60ead97
Serge Danzanvilliers <s.danzanvilliers@criteo.com>
2015-09-03 09:33:00 +0200

    Missing Apache License header
    Change-Id: I7df2ff18f5fc4c63f2e0fc8dba8e0687cee71131

--- kafka-sharp/kafka-sharp/Public/Statistics.cs
+++ kafka-sharp/kafka-sharp/Public/Statistics.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading;
+﻿// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+using System.Threading;
 
 namespace Kafka.Public
 {
