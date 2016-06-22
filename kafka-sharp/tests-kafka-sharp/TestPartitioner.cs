c231c55e0f307fe9e4c02734428b226009d827ee
Kevin Gosse <k.gosse@criteo.com>
2016-06-22 15:54:52 +0200

    Fixed two bugs in the PartitionSelector
    - The next partition wouldn't be properly selected after the current partition
    got blacklisted (only if delay was bigger than the number of partitions)
    - The conversion of _next to int could overflow and result in a negative number
    
    Change-Id: Icf1e8103eb4a4df09f503114db975e474eb1e3bd

--- kafka-sharp/tests-kafka-sharp/TestPartitioner.cs
+++ kafka-sharp/tests-kafka-sharp/TestPartitioner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Kafka.Public;
 using Kafka.Routing;
 using NUnit.Framework;
@@ -73,6 +74,95 @@ namespace tests_kafka_sharp
             Assert.AreEqual(1, partition.Id);
         }
 
+        /// <summary>
+        /// Make sure the round-robin threshold is reset when a partition is blacklisted half-way
+        /// </summary>
+        [Test]
+        public void TestRobinPartitionAssignWhenFiltered()
+        {
+            var nodeMock = new NodeMock();
+
+            var partitions = new[]
+            {
+                new Partition { Id = 1, Leader = nodeMock },
+                new Partition { Id = 2, Leader = nodeMock },
+                new Partition { Id = 3, Leader = nodeMock },
+            };
+
+            var filter = new Dictionary<int, DateTime>();
+
+            int delay = partitions.Length + 2;
+
+            var partitioner = new PartitionSelector(delay);
+
+            var partition = partitioner.GetPartition(Partitions.Any, partitions, filter);
+
+            Assert.AreEqual(1, partition.Id);
+
+            filter.Add(1, DateTime.UtcNow);
+
+            var batch = GetPartitions(delay, partitioner, partitions, filter);
+
+            Assert.AreEqual(delay, batch.Count);
+            Assert.IsTrue(batch.All(p => p.Id == 2), "The round-robin threshold wasn't properly reset after previous partition was blacklisted");
+        }
+
+        /// <summary>
+        /// Regression test: in some cases, the PartitionSelector could fail to find an available partition
+        /// if current partition was blacklisted and delay was greater than the number of partition
+        /// </summary>
+        [Test]
+        public void TestFilterWithHighDelay()
+        {
+            var nodeMock = new NodeMock();
+
+            var partitions = new[]
+            {
+                new Partition { Id = 1, Leader = nodeMock },
+                new Partition { Id = 2, Leader = nodeMock },
+                new Partition { Id = 3, Leader = nodeMock }
+            };
+
+            var filter = new Dictionary<int, DateTime> { { 2, DateTime.UtcNow } };
+
+            // Pick a delay greater than the number of partitions
+            int delay = partitions.Length + 2;
+
+            var partitioner = new PartitionSelector(delay);
+
+            var firstBatch = GetPartitions(delay, partitioner, partitions, filter);
+
+            Assert.AreEqual(delay, firstBatch.Count);
+            Assert.IsTrue(firstBatch.All(p => p.Id == 1));
+
+            var secondBatch = GetPartitions(delay, partitioner, partitions, filter);
+
+            Assert.AreEqual(delay, secondBatch.Count);
+            Assert.IsTrue(secondBatch.All(p => p.Id == 3));
+        }
+
+        /// <summary>
+        /// Test what happens when startSeed is high enough that _next will get bigger than int.MaxValue
+        /// This is purely meant as regression testing, as this condition caused issues in previous versions of the code
+        /// </summary>
+        [Test]
+        public void TestOverflow()
+        {
+            var nodeMock = new NodeMock();
+
+            var partitions = Enumerable.Range(0, 10).Select(i => new Partition { Id = i, Leader = nodeMock }).ToArray();
+
+            var partitioner = new PartitionSelector(1, int.MaxValue);
+
+            var batch = GetPartitions(partitions.Length, partitioner, partitions, null);
+
+            var ids = batch.Select(p => p.Id).ToArray();
+
+            var expectedIds = new[] { 7, 8, 9, 0, 1, 2, 3, 4, 5, 6 };
+
+            Assert.IsTrue(expectedIds.SequenceEqual(ids));
+        }
+
         [Test]
         [TestCase(0, 1)]
         [TestCase(1, 1)]
@@ -95,9 +185,21 @@ namespace tests_kafka_sharp
             {
                 for (var j = 0; j < delay; ++j)
                 {
-                    Assert.AreEqual((partition.Id + startSeed)%partitions.Length, partitioner.GetPartition(Partitions.Any, partitions).Id);
+                    Assert.AreEqual((partition.Id + startSeed) % partitions.Length, partitioner.GetPartition(Partitions.Any, partitions).Id);
                 }
             }
+        }
+
+        private static List<Partition> GetPartitions(int count, PartitionSelector partitioner, Partition[] partitions, Dictionary<int, DateTime> filter)
+        {
+            var result = new List<Partition>(count);
+
+            for (int i = 0; i < count; i++)
+            {
+                result.Add(partitioner.GetPartition(Partitions.Any, partitions, filter));
+            }
+
+            return result;
         }
     }
 }
